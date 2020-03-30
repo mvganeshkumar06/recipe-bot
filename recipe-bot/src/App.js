15aaf2b64cae083fcb4773b8240190d0959a8f51
Ganesh-Kumar6 <mvganeshk15@gmail.com>
2020-03-30 13:00:40 +0530

    UI Update - Responsive Design

--- recipe-bot/src/App.js
+++ recipe-bot/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import RecipeArray from './RecipeArray';
 import './App.css';
+import github from './Assets/github.jpg';
+import linkedin from './Assets/linkedin.png';
+import twitter from './Assets/twitter.png';
 import axios from 'axios';
 
 require('dotenv').config();
@@ -35,7 +38,6 @@ const App =()=>{
     
     else{
 
-      //setValidation('');
       console.log(response.data.results);
       setRecipes(response.data.results);
     
@@ -93,36 +95,76 @@ const App =()=>{
 
     <div className='app-container'>
         
-        <div>
+        <div className='app'>
                 
-            <h1 className='text'>Recipe Bot</h1>
+            <h1 className='text mainhead'>Recipe Bot</h1>
             
             <br/><br/>
                 
-            <input className='search' type='text' placeholder='Enter food name ...' onChange={getSearchValue} required ></input>
-            
-            <br/><br/><br/>
+            <input type='text' className='search' placeholder='Enter food name ...' onChange={getSearchValue} required ></input>
+
      
-            <h3 className='text'>Select number of recipes (1 to 10)</h3>
-            
-            <br/><br/>
+            <h3 className='text sliderhead'>Select number of recipes (1 to 10)</h3>
 
             <input className='slider' type='range' min='1' max='10' onChange={getSlideValue}/>
-            
-            <br/><br/>
+
+            <br/>
 
             <button className='btn' onClick={sendData}>Find me Recipes</button>
 
-            <br/> <br/>
-
             <button className='btn' type='reset' onClick={resetData}>Reset Values</button>
 
+            <br/> <br/>      
+            
+            <a href='https://spoonacular.com/food-api
+            ' target='_blank' rel='noopener noreferrer' className='attribution'> 
+            
+            POWERED BY SPOONACULAR API
+            
+            </a>
+            
             <br/> <br/>
-
+          
             
             {validation}   
 
             <RecipeArray values={recipes}/>
+
+        </div>
+
+        <div className='text infocard'>
+        
+                <h3 className='text note'>Note</h3>
+
+                Make sure you reset both the input field and slider before you search for recipes !
+
+        </div>
+
+        <div className='text devcard'>
+        
+                <h3 className='text note'>Developed by Ganesh Kumar</h3>
+                
+                <div className='connect'>
+                        <h3> You can find me on </h3>
+                        
+                        <a href='https://github.com/Ganesh-Kumar6' >
+                            
+                            <img src={github} alt='github'  className='icons' target='_blank' rel='noopener noreferer'/>
+                        
+                        </a> 
+                        
+                        <a href='https://www.linkedin.com/in/ganesh-kumar-700a26191/' >
+                            
+                            <img src={linkedin} alt='linkedin'  className='icons' target='_blank' rel='noopener noreferer'/>
+                        
+                        </a>
+                        
+                        <a href='https://twitter.com/Gannyv06' >
+                        
+                            <img src={twitter} alt='twitter' className='icons' target='_blank' rel='noopener noreferer'/>
+                        
+                        </a>
+                </div>
 
         </div>
 
