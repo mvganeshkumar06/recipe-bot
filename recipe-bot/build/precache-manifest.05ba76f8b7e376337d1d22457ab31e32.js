e0ff58761b3984a4a1c275fcbe11597f87d9e466
Ganesh-Kumar6 <mvganeshk15@gmail.com>
2020-03-31 10:55:14 +0530

    Added build folder

---
+++ recipe-bot/build/precache-manifest.05ba76f8b7e376337d1d22457ab31e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1097e2b35d1cd5ad1f5a6ef24181bffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d620a7d546f6f996a231",
+    "url": "/static/css/main.00eb03f6.chunk.css"
+  },
+  {
+    "revision": "7e8624f0632e0853261e",
+    "url": "/static/js/2.c1d80747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1d80747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d620a7d546f6f996a231",
+    "url": "/static/js/main.72173078.chunk.js"
+  },
+  {
+    "revision": "4f29fb742fc8ebb3e18d",
+    "url": "/static/js/runtime-main.f7e690f7.js"
+  },
+  {
+    "revision": "32bded9ad1b7f6a28629776d5757eacf",
+    "url": "/static/media/github.32bded9a.jpg"
+  }
+]);
