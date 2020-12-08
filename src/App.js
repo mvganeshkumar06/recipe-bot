b76718d424bf33e66ae34392646f7d201e8ccd80
Ganesh Kumar <mvganeshk15sde@gmail.com>
2020-12-08 12:09:22 +0530

    styles: updated styles

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
 
       if (!response.data.results || response.data.results.length === 0) {
         setValidation(
-          <p className="notFoundMessage">
+          <p className={css.notFoundMessage}>
             Sorry no food item found. Try something else
           </p>
         );
@@ -64,50 +64,103 @@ const App = () => {
 
   return (
     <AppContext.Provider value={recipes && { recipes, input }}>
-      <div className={css.inputContainer}>
-        <p className={css.head}>Recipe Bot</p>
-        <a
-          href="https://spoonacular.com/food-api
-              "
-          target="_blank"
-          rel="noopener noreferrer"
-          className={css.attribution}
-        >
-          Powered by Spoonacular API
-        </a>
-        <input
-          type="text"
-          name="searchValue"
-          className={css.search}
-          placeholder="Enter the food name"
-          value={input.searchValue}
-          onChange={updateInput}
-        />
-        <p className={css.sliderhead}>Select number of recipes [1 to 20]</p>
-        <input
-          type="range"
-          name="slideValue"
-          className={css.slider}
-          min="1"
-          max="20"
-          value={input.slideValue}
-          onChange={updateInput}
-        />
+      <div id={css.container}>
+        <div className={css.inputContainer}>
+          <p className={css.head}>Recipe Bot</p>
+          <input
+            type="text"
+            name="searchValue"
+            className={css.search}
+            placeholder="Enter the food name"
+            value={input.searchValue}
+            onChange={updateInput}
+          />
+          <p className={css.sliderhead}>Select number of recipes [1 to 20]</p>
+          <input
+            type="range"
+            name="slideValue"
+            className={css.slider}
+            min="1"
+            max="20"
+            value={input.slideValue}
+            onChange={updateInput}
+          />
 
-        <div className="btnContainer">
-          <button className={css.btn} onClick={() => validateAndFetch(input)}>
-            Find me recipes
+          <div className={css.btnContainer}>
+            <button className={css.btn} onClick={() => validateAndFetch(input)}>
+              Find me recipes
           </button>
 
-          <button className={css.btn} type="reset" onClick={resetData}>
-            Reset
+            <button className={css.btn} type="reset" onClick={resetData}>
+              Reset
           </button>
+          </div>
+
+          {validation}
         </div>
+        <div className={css.outputContainer}>
+          <RecipeList />
+        </div>
+        <footer>
+          <div id={css.socialFooter}>
+            <h4> Made with <img id={css.heartIcon} src="https://img.icons8.com/fluent/48/000000/like.png" alt="heart" /> by Ganesh Kumar</h4>
+            <a
+              href="https://mvganeshkumar.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={css.icons}
+                src="https://img.icons8.com/windows/64/000000/domain.png"
+                alt="website"
+              />
+            </a>
+            <a
+              href="https://github.com/Ganesh-Kumar6"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={css.icons}
+                src="https://img.icons8.com/fluent/48/000000/github.png"
+                alt="github"
+              />
+            </a>
 
-        {validation}
-      </div>
-      <div className={css.outputContainer}>
-        <RecipeList />
+            <a
+              href="https://www.linkedin.com/in/ganesh-kumar6"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={css.icons}
+                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+                alt="linkedin"
+              />
+            </a>
+            <a
+              href="https://dev.to/ganeshkumar6"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={css.icons}
+                src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
+                alt="dev"
+              />
+            </a>
+          </div>
+          <a
+            href="https://spoonacular.com/food-api
+              "
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css.iconAttribution}
+          >
+            Powered by Spoonacular API
+        </a>
+          <a className={css.iconAttribution} href="https://icons8.com">Icons by Icons8</a>
+        </footer>
       </div>
     </AppContext.Provider>
   );
